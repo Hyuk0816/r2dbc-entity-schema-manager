3d51cbe18bc2c25cc88fdcd6b8813678d24dd69c
Hyuk <rlawogur816@gmail.com>
2026-06-10 09:27:24 +0900

    Stop exporting Spring Boot dependency management

--- starter/build.gradle.kts
+++ starter/build.gradle.kts
@@ -1,9 +1,10 @@
 description = "Spring Boot starter for R2DBC Entity Schema Manager."
 
+val springBootVersion = "3.5.14"
+
 dependencies {
     api(project(":autoconfigure"))
     api(project(":core"))
 
-    api(platform("org.springframework.boot:spring-boot-dependencies:3.5.14"))
-    api("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    api("org.springframework.boot:spring-boot-starter-data-r2dbc:$springBootVersion")
 }
